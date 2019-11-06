ce80bffaeefb70335f02d318d6057541397aaf99
xiaohang89 <343341466@qq.com>
2019-11-06 13:33:13 +0800

    邮箱认证

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EnsureEmailIsVerified::class,      // <<--- 只需添加这一行  判断用户登录中间件
         ],
 
         'api' => [
