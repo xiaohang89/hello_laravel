764122e8330efa1a5d45673cd812a2f597db3807
xiaohang89 <343341466@qq.com>
2019-11-06 15:56:31 +0800

    密码重置

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
-// Email 认证相关路由
+// Email 认证相关路由   
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
