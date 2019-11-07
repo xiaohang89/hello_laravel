3cc4eb3ee2394cf291503a94d91cbbfb968a291a
xiaohang89 <343341466@qq.com>
2019-11-07 12:15:54 +0800

    授权访问

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class PagesController extends Controller
 {
-    public function root()
+    public function root(User $user)
     {
-        return view('pages.root');
+    	// 这里有问题获取不到图片
+        return view('pages.root',compact('user'));
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,18 +11,26 @@ use App\Models\User;
 use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
+    // 利用中间件方法，想要看编辑页面必须登录才可以 except 代表除了这个路由 以外的必须登录， only只有这个路由登录
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+    // 用户展示页面
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
-
+    // 用户修改页面
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
-
+    // 用户逻辑页面
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
