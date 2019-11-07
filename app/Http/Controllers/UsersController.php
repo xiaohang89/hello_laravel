2c156e4452abece22d3b78b113a1252505c4d38c
xiaohang89 <343341466@qq.com>
2019-11-07 10:49:52 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// 用户验证规则
 use App\Http\Requests\UserRequest;
 // 引用模型 
 use App\Models\User;
+//图片上传逻辑
+use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
     public function show(User $user)
@@ -18,9 +21,18 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
