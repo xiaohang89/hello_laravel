0b94c2fbddd26b4d80269a81f9457f4f667147d0
xiaohang89 <343341466@qq.com>
2019-11-08 12:18:11 +0800

    用户可以创建话题

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -18,4 +18,12 @@ class TopicObserver
     {
         //
     }
+
+    // creating, created, updating, updated, saving,
+    // saved,  deleting, deleted, restoring, restored
+ 	// Eloquent 观察器允许我们对给定模型中进行事件监控，观察者类里的方法名对应 Eloquent 想监听的事件。每种方法接收 model 作为其唯一的参数。代码生成器已经为我们生成了一个观察器文件，并在 AppServiceProvider 中注册。接下来我们要定制此观察器，在 Topic 模型保存时触发的 saving 事件中，对 excerpt 字段进行赋值：
+    public function saving(Topic $topic)
+    {
+        $topic->excerpt = make_excerpt($topic->body);
+    }
 }
--- app/helpers.php
+++ app/helpers.php
@@ -18,3 +18,10 @@ function category_nav_active($category_id)
 {
     return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
 }
+
+// 话题简介自动保存函数
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
+}
