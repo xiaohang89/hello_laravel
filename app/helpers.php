d498cc192005806934bb9f269cf27a97f8ffc264
xiaohang89 <343341466@qq.com>
2019-11-08 10:50:10 +0800

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -4,4 +4,17 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
-?>
+
+/**
+ * 如果 $condition 不为 False 即会返回字符串 `active`
+ *
+ * @param        $condition
+ * @param string $activeClass
+ * @param string $inactiveClass
+ *
+ * @return string
+ */
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,6 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 // Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 // Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+// 分类话题
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
