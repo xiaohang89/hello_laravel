4fa98f79247137a8d7cdb171fde3aec3fe5c6986
xiaohang89 <343341466@qq.com>
2019-11-08 13:39:46 +0800

    用户上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,6 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 
 // 分类话题
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+// 图片上传
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
