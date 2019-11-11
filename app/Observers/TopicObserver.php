f8ef90f5d4af172846c318d6fd0a6164e235dfa2
xiaohang89 <343341466@qq.com>
2019-11-11 14:42:29 +0800

    ““翻译队列话

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -7,6 +7,8 @@ use App\Models\Topic;
 use App\Handlers\SlugTranslateHandler;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
+// 消息列队
+use App\Jobs\TranslateSlug;
 
 class TopicObserver
 {
@@ -30,9 +32,21 @@ class TopicObserver
         // 自动填充简介、话题摘要
         $topic->excerpt = make_excerpt($topic->body);
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
-        if ( ! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
-        }
+        // if ( ! $topic->slug) {
+        //     $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        // }
 
+        
+    }
+
+    //模型监控器的 saved() 方法对应 Eloquent 的 saved 事件，此事件发生在创建和编辑时、数据入库以后。在 saved() 方法中调用，确保了我们在分发任务时，$topic->id 永远有值。
+    public function saved(Topic $topic)
+    {
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
+        }
     }
 }
