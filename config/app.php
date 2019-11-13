a259d8535d2434153b88a308e80fc7c622a1e6a8
xiaohang89 <343341466@qq.com>
2019-11-13 16:59:08 +0800

    用户管理后台

--- config/app.php
+++ config/app.php
@@ -176,6 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // 后台
+        Frozennode\Administrator\AdministratorServiceProvider::class,
 
     ],
 
