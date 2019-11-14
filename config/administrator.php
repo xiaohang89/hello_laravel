e7d1e20c8e3dca98bfe436b9c992acc3228528b0
xiaohang89 <343341466@qq.com>
2019-11-14 09:37:52 +0800

    内容管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -89,6 +89,12 @@ return array(
             'roles',
             'permissions',
         ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
+        
     ],
 
     /*
